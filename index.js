530971aff6aee67e258e83d56af4a3ca14e789f4
aidinia <ai.sorgen@gmail.com>
2021-02-04 19:01:11 +0000

    typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
 http.createServer(function (request, response){
-    response.wrhiteHead(200, {'Content-Type': "text/plain"});
+    response.writeHead(200, {'Content-Type': "text/plain"});
     response.end("Hello WOrld\n");
 }).listen(8080);
 
