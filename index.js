d4c6a688beb0fd997d0fa414ea3fe98e77ed4af2
aidinia <ai.sorgen@gmail.com>
2021-02-04 22:26:20 +0000

    add port

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ var http = require("http");
 
 const PORT = process.env.PORT || 3000;
 
-//app.listen(PORT, () => {
- //   console.log("app running on port" + PORT);
-//});
+
 
 http.createServer(function (request, response){
     response.writeHead(200, {'Content-Type': "text/plain"});
     response.end("Hello WOrld");
-}).listen(8080);
+}).listen(PORT);
 
 console.log("Server running");
