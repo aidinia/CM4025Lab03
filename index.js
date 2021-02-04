b9007eacb6f3fb4626e456c4cf11be391ecf70cb
aidinia <ai.sorgen@gmail.com>
2021-02-04 22:17:27 +0000

    app vs http

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var http = require("http");
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log("app running on port"+ PORT);
+
+http.listen(PORT, () => {
+    console.log("app running on port" + PORT);
 });
 
 http.createServer(function (request, response){
     response.writeHead(200, {'Content-Type': "text/plain"});
-    response.end("Hello WOrld\n");
+    response.end("Hello WOrld");
 }).listen(8080);
 
 console.log("Server running");
