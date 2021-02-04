a3c707064f29b585c624bc67d67c53e8ff337194
aidinia <ai.sorgen@gmail.com>
2021-02-04 22:55:31 +0000

    server.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+const { fstat } = require("fs");
 var http = require("http");
-
+fs.require('fs');
 const PORT = process.env.PORT || 3000;
 
-
-
 http.createServer(function (request, response){
-    response.writeHead(200, {'Content-Type': "text/plain"});
-    response.end("Hello WOrld");
+
+    response.writeHead(200, {
+        'Content-Type': "text/plain",
+    'Acces-Control-Allow-Origin' : '*'
+});
+    var readStream = fs.createReadStream(__dirname, "/index.html");
+    readStream.pipe(response);
 }).listen(PORT);
 
 console.log("Server running");
