0d02b0ad520f1e0ba99fb83d56d1cec0bf518d31
aidinia <ai.sorgen@gmail.com>
2021-02-04 22:21:09 +0000

    comment out app

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var http = require("http");
 
 const PORT = process.env.PORT || 3000;
 
-http.listen(PORT, () => {
-    console.log("app running on port" + PORT);
-});
+//app.listen(PORT, () => {
+ //   console.log("app running on port" + PORT);
+//});
 
 http.createServer(function (request, response){
     response.writeHead(200, {'Content-Type': "text/plain"});
