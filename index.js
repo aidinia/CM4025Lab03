ccebb3882db134ba37e9a52204ae9248c66f559e
aidinia <ai.sorgen@gmail.com>
2021-02-04 19:46:38 +0000

    coma

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require("http");
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("app running on port ${ PORT}");
+    console.log("app running on port"+ PORT);
 });
 
 http.createServer(function (request, response){
